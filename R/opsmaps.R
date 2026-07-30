8642bf1949f1c2ebf71e7f887fef32c1b60e0a7f
Meixi Lin <22927483+meixilin@users.noreply.github.com>
2026-07-30 14:52:52 -0700

    Revert some files to 858eb44

--- R/opsmaps.R
+++ R/opsmaps.R
@@ -6,24 +6,13 @@
     return(invisible())
 }
 
-# coerce a samplemap raster to a live terra::SpatRaster, whatever form it
-# arrives in: PackedSpatRaster (unwrap), already a SpatRaster (no-op, since
-# terra::rast() on an existing SpatRaster drops its values), or a legacy
-# raster::RasterLayer (rast() converts it without losing values).
-.as_spatraster <- function(rr) {
-    if (inherits(rr, "PackedSpatRaster")) terra::unwrap(rr)
-    else if (inherits(rr, "SpatRaster")) rr
-    else terra::rast(rr)
-}
-
 # calculate area of a given raster
 .areaofraster <- function(rr, tol = 5) {
-    rr <- .as_spatraster(rr)
     aa <- terra::cellSize(rr, unit = "km")
     # check if cells' coefficient of variation too large (different area per cell)
     # not using any na.rm = TRUE here because aa should not have any NA in any cases
-    cv <- terra::global(aa, "sd")[1, 1] / terra::global(aa, "mean")[1, 1] * 100
-    if (is.na(cv)) cv <- 0 # TODO: seemed to be a bug in terra::global if all values are the same
+    cv <- terra::global(aa, "sd")[1,1] / terra::global(aa, "mean")[1,1] * 100
+    if (is.na(cv)) cv = 0 # TODO: seemed to be a bug in terra::global if all values are the same
     # if cv > tol/100, warn the variation
     if (cv > tol) {
         warning(paste("Given projection distorts area. Area coefficient of variation =", round(cv, 1), "%"))
@@ -46,7 +35,6 @@
 .rowcol_cellid <- function(sm, bbox, revbbox = FALSE) {
     stopifnot(length(bbox) == 4)
     # get the cells
-    sm <- .as_spatraster(sm)
     cells <- terra::cellFromRowColCombine(sm, bbox[1]:bbox[2], bbox[3]:bbox[4])
     # reverse the cells if revbbox
     if (revbbox) {
@@ -55,3 +43,4 @@
     cellsnotna <- intersect(terra::cells(sm), cells)
     return(cellsnotna)
 }
+
