b3d0371278e40239fbab38d9b1465b13ec63cbd4
sawyerth <sawyerthomsen15@berkeley.edu>
2026-07-30 11:53:50 -0700

    vignette fixes

--- R/MARsampling.R
+++ R/MARsampling.R
@@ -44,7 +44,7 @@ MARsampling <-
         # TODO: user specified central point
         sm <- .as_spatraster(sm)
         maxrc <- terra::where.max(sm) # can have multiple rows when tied
-        r0c0 <- terra::rowColFromCell(sm, maxrc[1, 'cell'])
+        r0c0 <- terra::rowColFromCell(sm, maxrc[1, "cell"])
         # calculate and store non-empty cell once, as a summed-area table for O(1) box occupancy queries
         pres <- !is.na(terra::as.matrix(sm, wide = TRUE))
         cumocc <- apply(pres, 2, cumsum)
